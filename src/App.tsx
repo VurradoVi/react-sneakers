d8cd5aab92e989a42e0f6c733e3c9a09fdf82d42
Vlad <02kvg03@mail.ru>
2024-11-09 23:00:07 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+
+
+function App() {
+  return (
+    <div className="wrapper">
+      <header>
+        <div className="headerLeft">
+          <img width={40} height={40} src="/img/logo.png" />
+          <div className="headerInfo">
+            <h3>React Sneakers</h3>
+            <p>Магазин лучших кроссовок</p>
+          </div>
+        </div>
+        <ul className="headerRight">
+          <li>
+          <img width={18} height={18} src="/img/trash.svg" />
+            <span>1205 руб.</span>
+          </li>
+          <li>
+          <img width={18} height={18} src="/img/User.svg" />
+          </li>
+        </ul>
+      </header>
+      <div className="content">
+        <h1>Все кроссовки</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
